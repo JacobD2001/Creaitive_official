5aceff41f09d4ccc413cdd574eca770e8e0b41c2
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-04 22:25:54 +0100

    basic structure of the app

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,13 +1,34 @@
+//librairies
 import React from "react";
+import styled from "styled-components";
+
+//components
 import Navbar from "../navbar";
 import Intro from "../intro";
+import Footer from "../footer";
+import Contact from "../contact";
+import FAQ from "../faq";
+import Services from "../services";
+import Recommend from "../recommend";
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+}
+`
 
 function Home() {
   return (
-    <div>
+    <Container>
       <Navbar />
       <Intro />
-    </div>
+      <Services />
+      <Contact />
+      <Recommend />
+      <FAQ />
+      <Footer />
+    </Container>
   );
 }
 
