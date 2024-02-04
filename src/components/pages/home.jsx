063f495f0984d687f13974e5ad8ed4dcd9d971d5
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-04 22:17:59 +0100

    first commit

---
+++ src/components/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar";
+import Intro from "../intro";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+    </div>
+  );
+}
+
+export default Home;
