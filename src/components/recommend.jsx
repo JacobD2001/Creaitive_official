88f1b890e0894238cde4e68cf05e9dc89f43f94f
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-08 13:20:38 +0100

    recommend bar animation fixed

--- src/components/recommend.jsx
+++ src/components/recommend.jsx
@@ -5,20 +5,22 @@ import styled, { keyframes } from 'styled-components';
 const moveLeftToRight = keyframes`
   0% {
     transform: translateX(-100%);
+    opacity: 1; // Start with the element fully visible
   }
 
   50% {
     transform: translateX(100%);
-    opacity: 0; /* Fade out halfway through the animation */
+    opacity: 0; // Fade out halfway through the animation
   }
 
   51% {
-    transform: translateX(-100%);
-    opacity: 0; /* Start fading in immediately after reaching the right side */
+    transform: translateX(100%);
+    opacity: 0; // Keep the element on the right side and invisible
   }
 
   100% {
-    transform: translateX(100%);
+    transform: translateX(-100%);
+    opacity: 1; // Move the element to the left and fade it back in
   }
 `;
 
