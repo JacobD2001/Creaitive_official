f5b79b397ee66ff1d0d5e4699edc67deb0a2ea0b
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-16 20:16:24 +0100

    strony cleaned

--- src/components/services_components/stronyInternetowe.jsx
+++ src/components/services_components/stronyInternetowe.jsx
@@ -1,26 +1,14 @@
 import React from "react";
-import { WorkComponentContainer, KeyPointsContainer, KeyPointColumn, Button, Title, Subtitle } from "../styled_components/workComponentServicesStyledComponents";
-// import styled, { keyframes } from "styled-components";
+import {
+  WorkComponentContainer,
+  KeyPointsContainer,
+  KeyPointColumn,
+  Button,
+  Title,
+  Subtitle,
+} from "../styled_components/workComponentServicesStyledComponents";
 
-// const pulseAnimation = keyframes`
-//   0% {
-//     transform: scale(1);
-//     box-shadow: 0 0 0 0px rgba(218, 78, 162, 0.7);
-//   }
-//   50% {
-//     transform: scale(1.05);
-//     box-shadow: 0 0 0 15px rgba(218, 78, 162, 0);
-//   }
-//   100% {
-//     transform: scale(1);
-//     box-shadow: 0 0 0 0px rgba(218, 78, 162, 0.7);
-//   }
-// `;
-
-
-
-const StronyInternetowe = ({contactRef}) => {
-
+const StronyInternetowe = ({ contactRef }) => {
   const scrollToContact = () => {
     if (contactRef && contactRef.current) {
       contactRef.current.scrollIntoView({ behavior: "smooth" });
@@ -29,20 +17,16 @@ const StronyInternetowe = ({contactRef}) => {
 
   return (
     <WorkComponentContainer>
-
       <KeyPointsContainer>
+
         <KeyPointColumn>
-          {/* Key Point 1 */}
           <Title>Projekt </Title>
           <Subtitle>
             Wykonamy projekt strony internetowej szyty na miarę twojego biznesu
           </Subtitle>
         </KeyPointColumn>
 
-        <KeyPointColumn
-         
-        >
-          {/* Key Point 2 */}
+        <KeyPointColumn>
           <Title>Funkcjonalność</Title>
           <Subtitle>
             Sprawimy aby twoja strona internetowa była w pełni funkcjonalna
@@ -50,7 +34,6 @@ const StronyInternetowe = ({contactRef}) => {
         </KeyPointColumn>
 
         <KeyPointColumn>
-          {/* Key Point 3 */}
           <Title>Personalizacja</Title>
           <Subtitle>
             Dostosujemy funkcjonalność witryny internetowej w pełni do twoich
@@ -58,8 +41,7 @@ const StronyInternetowe = ({contactRef}) => {
           </Subtitle>
         </KeyPointColumn>
 
-        <KeyPointColumn >
-          {/* Key Point 3 */}
+        <KeyPointColumn>
           <Title>Responsywność</Title>
           <Subtitle>
             Stworzymy w pełni responsywną stronę, dopasowaną do każdego
@@ -67,19 +49,12 @@ const StronyInternetowe = ({contactRef}) => {
           </Subtitle>
         </KeyPointColumn>
 
-{/* <KeyPointColumn> */}
-        <Button onClick={() => scrollToContact(contactRef)}>Porozmawiajmy</Button>
-        {/* </KeyPointColumn> */}
-        {/* <KeyPointColumn>
-        <Button onClick={() => scrollToContact(contactRef)}>Dowiedz się więcej</Button>
-        </KeyPointColumn> */}
+        <Button onClick={() => scrollToContact(contactRef)}>
+          Porozmawiajmy
+        </Button>
 
       </KeyPointsContainer>
-
-      {/* Call to Action Button */}
     </WorkComponentContainer>
   );
 };
 export default StronyInternetowe;
-
-
