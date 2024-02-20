e0e3b36e7b83deda09afe8ed282b22687179a115
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-20 19:19:24 +0100

    serviced finished

--- src/components/services_components/indywidualneRozwiazania.jsx
+++ src/components/services_components/indywidualneRozwiazania.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import {
+  WorkComponentContainer,
+  KeyPointsContainer,
+  KeyPointColumn,
+  Button,
+  Title,
+  Subtitle,
+} from "../styled_components/workComponentServicesStyledComponents";
 
-function IndywidualneRozwiazania() {
-    return (
-        <div>indywidualne rozwiazania</div>
-    )
-}
+const indywidualneRozwiazania = ({ contactRef }) => {
+  const scrollToContact = () => {
+    if (contactRef && contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-export default IndywidualneRozwiazania
+  return (
+    <WorkComponentContainer>
+      <KeyPointsContainer>
+        <KeyPointColumn>
+          <Title>Personalizacja</Title>
+          <Subtitle>
+            Kreujemy indywidualne rozwiązania, dostosowane do
+            Twoich potrzeb 
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Analiza</Title>
+          <Subtitle>
+            Przeprowadzimy szczegółową analizę, aby zrozumieć Twoje potrzeby
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Elastyczność</Title>
+          <Subtitle>
+            Stworzymy elastyczne rozwiązania, które będą rozwijać się wraz z Twoją firmą
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Innowacje</Title>
+          <Subtitle>
+            Wdrażamy najnowsze technologie, aby Twoja firma była na bieżąco
+          </Subtitle>
+        </KeyPointColumn>
+
+        <Button onClick={() => scrollToContact(contactRef)}>
+          Porozmawiajmy
+        </Button>
+      </KeyPointsContainer>
+    </WorkComponentContainer>
+  );
+};
+export default indywidualneRozwiazania;
