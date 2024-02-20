097b3b5fb1c82930902946a8bde7682b4f032188
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-20 16:36:54 +0100

    optymalizacja procesów ai

--- src/components/services_components/AIOptymalizacjaProcesow.jsx
+++ src/components/services_components/AIOptymalizacjaProcesow.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import {
+  WorkComponentContainer,
+  KeyPointsContainer,
+  KeyPointColumn,
+  Button,
+  Title,
+  Subtitle,
+} from "../styled_components/workComponentServicesStyledComponents";
 
-function AIOptymalizacjaProcesow() {
-    return (
-        <div>optym procesow</div>
-    )
-}
+const AIOptymalizacjaProcesow = ({ contactRef }) => {
+  const scrollToContact = () => {
+    if (contactRef && contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-export default AIOptymalizacjaProcesow
+  return (
+    <WorkComponentContainer>
+      <KeyPointsContainer>
+        <KeyPointColumn>
+          <Title>Analiza Procesów</Title>
+          <Subtitle>
+            Dokładnie przeanalizujemy procesy w Twojej firmie, aby zidentyfikować obszary do optymalizacji
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Optymalizacja</Title>
+          <Subtitle>
+            Zoptymalizujemy istniejące procesy, eliminując zbędne kroki i
+            identyfikując obszary do usprawnienia
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Automatyzacja</Title>
+          <Subtitle>
+            Wprowadzimy rozwiązania oparte na sztucznej inteligencji, aby
+            zautomatyzować powtarzalne zadania i przyspieszyć procesy
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Monitorowanie</Title>
+          <Subtitle>
+            Stworzymy narzędzia do monitorowania efektywności optymalizacji oraz
+            generowania raportów dla lepszego zarządzania twoim biznesem
+          </Subtitle>
+        </KeyPointColumn>
+
+        <Button onClick={() => scrollToContact(contactRef)}>
+          Porozmawiajmy
+        </Button>
+      </KeyPointsContainer>
+    </WorkComponentContainer>
+  );
+};
+export default AIOptymalizacjaProcesow;
--- src/components/services_components/chatboty.jsx
+++ src/components/services_components/chatboty.jsx
@@ -17,43 +17,43 @@ const Chatboty = ({ contactRef }) => {
 
   return (
     <WorkComponentContainer>
-  <KeyPointsContainer>
+      <KeyPointsContainer>
+        <KeyPointColumn>
+          <Title>Implementacja</Title>
+          <Subtitle>
+            Wdrożymy inteligentnego chatbota dostosowanego do Twojej strony lub
+            komunikatora
+          </Subtitle>
+        </KeyPointColumn>
 
-<KeyPointColumn>
-  <Title>Implementacja</Title>
-  <Subtitle>
-    Wdrożymy inteligentnego chatbota dostosowanego do Twojej strony lub
-    komunikatora
-  </Subtitle>
-</KeyPointColumn>
+        <KeyPointColumn>
+          <Title>Personalizacja</Title>
+          <Subtitle>
+            Dostosujemy chatbota do unikalnych potrzeb Twojej marki
+          </Subtitle>
+        </KeyPointColumn>
 
-<KeyPointColumn>
-  <Title>Personalizacja</Title>
-  <Subtitle>
-    Dostosujemy chatbota do unikalnych potrzeb Twojej marki
-  </Subtitle>
-</KeyPointColumn>
+        <KeyPointColumn>
+          <Title>Szkolenie</Title>
+          <Subtitle>
+            Wyuczmy chatbota, aby odpowiadał na pytania klientów zgodnie z
+            twoimi potrzebami
+          </Subtitle>
+        </KeyPointColumn>
 
-<KeyPointColumn>
-  <Title>Szkolenie</Title>
-  <Subtitle>
-    Wyuczmy chatbota, aby odpowiadał na pytania klientów zgodnie z twoimi potrzebami
-  </Subtitle>
-</KeyPointColumn>
+        <KeyPointColumn>
+          <Title>Integracja</Title>
+          <Subtitle>
+            Zintegrujemy chatbota z różnymi platformami komunikacyjnymi, takimi
+            jak Messenger, WhatsApp itp.
+          </Subtitle>
+        </KeyPointColumn>
 
-<KeyPointColumn>
-  <Title>Integracja</Title>
-  <Subtitle>
-    Zintegrujemy chatbota z różnymi platformami komunikacyjnymi, takimi jak
-    Messenger, WhatsApp itp.
-  </Subtitle>
-</KeyPointColumn>
-
-<Button onClick={() => scrollToContact(contactRef)}>
-  Porozmawiajmy
-</Button>
-
-</KeyPointsContainer>    </WorkComponentContainer>
+        <Button onClick={() => scrollToContact(contactRef)}>
+          Porozmawiajmy
+        </Button>
+      </KeyPointsContainer>
+    </WorkComponentContainer>
   );
 };
 export default Chatboty;
