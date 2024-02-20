ad39fa1fb67e5047dd34df876c719dba36aad456
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-02-20 19:13:39 +0100

    influ

--- src/components/services_components/wirtualniInfluencerzy.jsx
+++ src/components/services_components/wirtualniInfluencerzy.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  WorkComponentContainer,
+  KeyPointsContainer,
+  KeyPointColumn,
+  Button,
+  Title,
+  Subtitle,
+} from "../styled_components/workComponentServicesStyledComponents";
 
-function wirtualniInfluencerzy() {
-    return (
-        <div>influencerki</div>
-    )
-}
+const wirtualniInfluencerzy = ({ contactRef }) => {
+  const scrollToContact = () => {
+    if (contactRef && contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-export default wirtualniInfluencerzy
+  return (
+    <WorkComponentContainer>
+      <KeyPointsContainer>
+        <KeyPointColumn>
+          <Title>Personalizacja</Title>
+          <Subtitle>
+            Influencerzy dostosowani do wizerunku i wartości Twojej marki
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Specjalizacja</Title>
+          <Subtitle>
+            Ty decydujesz o niszy i charakterze wirtualnego influencera
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Followersi</Title>
+          <Subtitle>
+            Zapewniamy budowę bazy followersów zgodnie z wytycznymi klienta
+          </Subtitle>
+        </KeyPointColumn>
+
+        <KeyPointColumn>
+          <Title>Ambasador Marki</Title>
+          <Subtitle>
+            Tworzymy twarz twojej marki, która promuje produkty i usługi
+          </Subtitle>
+        </KeyPointColumn>
+
+        <Button onClick={() => scrollToContact(contactRef)}>
+          Porozmawiajmy
+        </Button>
+      </KeyPointsContainer>
+    </WorkComponentContainer>
+  );
+};
+export default wirtualniInfluencerzy;
